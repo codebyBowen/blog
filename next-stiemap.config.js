e5f682c01862d902bb6e9a1218ca1f16e64aba78
Bowen Jiang <106635336+codebyBowen@users.noreply.github.com>
2025-02-27 20:13:59 +1100

    add sitemap

---
+++ next-stiemap.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteUrl: 'https://thebowvee.com/',
+    generateRobotsTxt: true,
+    // Optional configurations
+    changefreq: 'weekly',
+    priority: 0.7,
+    sitemapSize: 5000,
+    exclude: ['/admin/*', '/private/*'],
+  }
